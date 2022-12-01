9f3dfe2b1b00b7f9b2791ab0accd3dc4fc65d494
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2022-12-01 19:57:17 +0100

    :art: Apply Prettier

--- andrew/src/01/01.ts
+++ andrew/src/01/01.ts
@@ -1,10 +1,7 @@
 export const countCalories = ([calories = 0, ...inventory]: number[]): number =>
     calories + (inventory.length && countCalories(inventory))
 
-export const sumTopNCalorieCounts = (
-    inventories: number[][],
-    n: number = 1
-): number =>
+export const sumTopNCalorieCounts = (inventories: number[][], n: number = 1): number =>
     countCalories(
         inventories
             .map(countCalories)
