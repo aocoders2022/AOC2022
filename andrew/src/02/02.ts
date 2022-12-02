2ec301be7aba912aa2da0ee6f3e7ce0cb40046e9
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2022-12-02 09:43:51 +0100

    :recycle: Simplify condition

--- andrew/src/02/02.ts
+++ andrew/src/02/02.ts
@@ -8,10 +8,9 @@ export const calculateTotalScore = (
     (rounds.length && calculateTotalScore(rounds, applyInstructions))
 
 export const followInstructions = ([abc, xyz]: Round): number =>
-    ["X", "Y", "Z"].indexOf(xyz) * 3 +
-    { AX: 3, AY: 1, AZ: 2, BX: 1, BY: 2, BZ: 3, CX: 2, CY: 3, CZ: 1 }[`${abc}${xyz}`]
+    ({ X: 0, Y: 3, Z: 6 }[xyz] +
+    { AX: 3, AY: 1, AZ: 2, BX: 1, BY: 2, BZ: 3, CX: 2, CY: 3, CZ: 1 }[`${abc}${xyz}`])
 
 export const guessInstructions = ([abc, xyz]: Round): number =>
-    ["X", "Y", "Z"].indexOf(xyz) +
-    1 +
-    { AX: 3, AY: 6, AZ: 0, BX: 0, BY: 3, BZ: 6, CX: 6, CY: 0, CZ: 3 }[`${abc}${xyz}`]
+    ({ X: 1, Y: 2, Z: 3 }[xyz] +
+    { AX: 3, AY: 6, AZ: 0, BX: 0, BY: 3, BZ: 6, CX: 6, CY: 0, CZ: 3 }[`${abc}${xyz}`])
