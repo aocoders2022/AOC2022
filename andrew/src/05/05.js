ccc18925646987f2a5a6e791d979dcef8473b82f
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2022-12-05 13:30:26 +0100

    Clean up function API

--- andrew/src/05/05.js
+++ andrew/src/05/05.js
@@ -1,21 +1,32 @@
-import { addIndex, filter, gt, identity, join, lte, map, nth, nthArg, pipe, reverse } from "ramda"
+import {
+    addIndex,
+    curry,
+    filter,
+    gt,
+    identity,
+    join,
+    lte,
+    map,
+    nth,
+    nthArg,
+    pipe,
+    reverse,
+} from "ramda"
 
-const applyMove =
-    ([count, from, to], stacks, transformation) =>
-    (stack, i) =>
-        i === from - 1
-            ? addIndex(filter)(pipe(nthArg(1), lte(count)), stack)
-            : i === to - 1
-            ? [
-                  ...transformation(
-                      reverse(addIndex(filter)(pipe(nthArg(1), gt(count)), stacks[from - 1]))
-                  ),
-                  ...stack,
-              ]
-            : stack
+const applyMove = ([count, from, to], stacks, transformation, stack, i) =>
+    i === from - 1
+        ? addIndex(filter)(pipe(nthArg(1), lte(count)), stack)
+        : i === to - 1
+        ? [
+              ...transformation(
+                  reverse(addIndex(filter)(pipe(nthArg(1), gt(count)), stacks[from - 1]))
+              ),
+              ...stack,
+          ]
+        : stack
 
 export const crateMover9000 = ([count, from, to], stacks, transformation = identity) =>
-    addIndex(map)(applyMove([count, from, to], stacks, transformation), stacks)
+    addIndex(map)(curry(applyMove)([count, from, to], stacks, transformation), stacks)
 
 export const crateMover9001 = ([count, from, to], stacks) =>
     crateMover9000([count, from, to], stacks, reverse)
