06a459537b2ae8290e27c7b1fe61ccb4a5aa24ad
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2022-12-10 13:33:53 +0100

    Clean up a little

--- andrew/src/10/10.js
+++ andrew/src/10/10.js
@@ -13,67 +13,35 @@ export const findCycle = (instructions, [cycle, { x }], at) =>
 
             const nextCurrent = completeCycle(current, { action, value })
 
-            if (nextCurrent[0] > at) {
-                return current
-            }
-
-            return nextCurrent
+            return nextCurrent[0] > at ? current : nextCurrent
         },
         [cycle, { x }]
     )
 
 export const findSignalStrength = (instructions, [cycle, { x }], at) =>
-    instructions.reduce(
-        (current, { action, value }) => {
-            if (current[0] === at) {
-                return current
-            }
+    findCycle(instructions, [cycle, { x }], at)[1].x * at
 
-            const nextCurrent = completeCycle(current, { action, value })
-
-            if (nextCurrent[0] > at) {
-                return current
-            }
-
-            return nextCurrent
-        },
-        [cycle, { x }]
-    )[1].x * at
-
-export const renderCRT = (instructions, [cycle, { x }]) => {
-    const screen = Array(6)
+export const renderCRT = (instructions, [cycle, { x }]) =>
+    "\n" +
+    Array(6)
         .fill(null)
         .map((_, i) =>
             Array(40)
                 .fill(null)
                 .map((_, j) => [j, i * 40 + j + 1])
         )
+        .map((line) =>
+            line
+                .map(([i, pixel]) => {
+                    const atCycle = findCycle(instructions, [cycle, { x }], pixel)
 
-    return (
-        "\n" +
-        screen
-            .map((line, j) =>
-                line
-                    .map(([i, pixel]) => {
-                        const pixelCycle = pixel
-
-                        const atCycle = findCycle(instructions, [cycle, { x }], pixelCycle)
-
-                        if (
-                            atCycle[1].x === i ||
-                            atCycle[1].x + 1 === i ||
-                            atCycle[1].x - 1 === i
-                        ) {
-                            return "#"
-                        }
-
-                        return "."
-                    })
-                    .join("")
-            )
-            .join("\n")
-    )
-}
+                    return atCycle[1].x === i || atCycle[1].x + 1 === i || atCycle[1].x - 1 === i
+                        ? "#"
+                        : "."
+                })
+                .join("")
+        )
+        .join("\n")
 
 export const sumSignalStrengths = (instructions, [cycle, { x }], ats) =>
     ats.map(findSignalStrength.bind(null, instructions, [cycle, { x }])).reduce((a, b) => a + b)
