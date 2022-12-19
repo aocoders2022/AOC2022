1f8a9aa454d741c69ebd800cea53a4a301a0627e
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2022-12-19 21:50:27 +0100

    Complete day 18 part 2

--- andrew/src/18/18.js
+++ andrew/src/18/18.js
@@ -1,20 +1,124 @@
+export const countExteriorSurfaces = (plottedDropletScan) =>
+    countUnconnectedSurfaces(plottedDropletScan) -
+    countUnconnectedSurfaces(getTrappedPockets(plottedDropletScan))
+
 export const countUnconnectedSurfaces = (plottedDropletScan) =>
     Array.from(plottedDropletScan)
         .map((xyz) => xyz.split(",").map(Number))
         .map(([x, y, z]) => ({ x, y, z }))
         .map(
             ({ x, y, z }) =>
-                [
-                    { x, y: y + 1, z },
-                    { x, y: y - 1, z },
-                    { x: x + 1, y, z },
-                    { x: x - 1, y, z },
-                    { x, y, z: z + 1 },
-                    { x, y, z: z - 1 },
-                ].filter(({ x: x1, y: y1, z: z1 }) => !plottedDropletScan.has(`${x1},${y1},${z1}`))
-                    .length
+                getSiblings({ x, y, z }).filter(
+                    ({ x: x1, y: y1, z: z1 }) => !plottedDropletScan.has(`${x1},${y1},${z1}`)
+                ).length
         )
         .reduce((n1, n2) => n1 + n2)
+
+export const getSiblings = ({ x, y, z }) => [
+    { x, y: y + 1, z },
+    { x, y: y - 1, z },
+    { x: x + 1, y, z },
+    { x: x - 1, y, z },
+    { x, y, z: z + 1 },
+    { x, y, z: z - 1 },
+]
+
+export const getTrappedPockets = (plottedDropletScan) => {
+    const plottedCoordinates = Array.from(plottedDropletScan)
+        .map((xyz) => xyz.split(",").map(Number))
+        .map(([x, y, z]) => ({ x, y, z }))
+
+    const xCoordinates = plottedCoordinates.map(({ x }) => x)
+    const yCoordinates = plottedCoordinates.map(({ y }) => y)
+    const zCoordinates = plottedCoordinates.map(({ z }) => z)
+
+    const minX = Math.min(...xCoordinates)
+    const maxX = Math.max(...xCoordinates)
+    const xSliceCount = maxX - minX + 1
+
+    const minY = Math.min(...yCoordinates)
+    const maxY = Math.max(...yCoordinates)
+    const ySliverCount = maxY - minY + 1
+
+    const minZ = Math.min(...zCoordinates)
+    const maxZ = Math.max(...zCoordinates)
+    const zBlockCount = maxZ - minZ + 1
+
+    const cubeCoordinates = Array.from(Array(xSliceCount), (_, i) => i + minX).flatMap((x) =>
+        Array.from(Array(ySliverCount), (_, j) => j + minY).flatMap((y) =>
+            Array.from(Array(zBlockCount), (_, k) => k + minZ).map((z) => ({ x, y, z }))
+        )
+    )
+
+    const potentiallyTrappedPockets = cubeCoordinates
+        .filter(({ x, y, z }) => !plottedDropletScan.has(`${x},${y},${z}`))
+        .filter(({ x, y, z }) => isEnclosed({ x, y, z }, plottedCoordinates))
+
+    const trappedPockets = potentiallyTrappedPockets.filter(({ x, y, z }, j) => {
+        const stillToCheck = [{ x, y, z }]
+        const alreadyFetched = new Set([`${x},${y},${z}`])
+
+        while (stillToCheck.length) {
+            const nextToCheck = stillToCheck.shift()
+
+            const siblings = getSiblings(nextToCheck)
+
+            const emptySiblings = siblings.filter(
+                (sibling) =>
+                    !plottedDropletScan.has(`${sibling.x},${sibling.y},${sibling.z}`) &&
+                    !alreadyFetched.has(`${sibling.x},${sibling.y},${sibling.z}`)
+            )
+
+            emptySiblings.forEach((es) => {
+                stillToCheck.push(es)
+                alreadyFetched.add(`${es.x},${es.y},${es.z}`)
+            })
+
+            if (!isEnclosed(nextToCheck, plottedCoordinates)) {
+                return false
+            }
+        }
+
+        return true
+    })
+
+    return new Set(trappedPockets.map(({ x, y, z }) => `${x},${y},${z}`))
+}
+
+export const isEnclosed = ({ x, y, z }, plottedCoordinates) => {
+    const hasSiblingsLeft = plottedCoordinates.some(
+        (coordinates) => coordinates.y === y && coordinates.z === z && coordinates.x < x
+    )
+
+    const hasSiblingsRight = plottedCoordinates.some(
+        (coordinates) => coordinates.y === y && coordinates.z === z && coordinates.x > x
+    )
+
+    const hasSiblingsAbove = plottedCoordinates.some(
+        (coordinates) => coordinates.y > y && coordinates.z === z && coordinates.x === x
+    )
+
+    const hasSiblingsBelow = plottedCoordinates.some(
+        (coordinates) => coordinates.y < y && coordinates.z === z && coordinates.x === x
+    )
+
+    const hasSiblingsFront = plottedCoordinates.some(
+        (coordinates) => coordinates.y === y && coordinates.z > z && coordinates.x === x
+    )
+
+    const hasSiblingsBehind = plottedCoordinates.some(
+        (coordinates) => coordinates.y === y && coordinates.z < z && coordinates.x === x
+    )
+
+    return (
+        hasSiblingsLeft &&
+        hasSiblingsRight &&
+        hasSiblingsAbove &&
+        hasSiblingsBelow &&
+        hasSiblingsFront &&
+        hasSiblingsBehind
+    )
+}
 
 export const parseDropletScan = (input) =>
     input
@@ -22,5 +126,8 @@ export const parseDropletScan = (input) =>
         .map((line) => line.split(",").map(Number))
         .map(([x, y, z]) => ({ x, y, z }))
 
-export const plotDropletScan = (droplets, grid) =>
-    droplets.reduce((newGrid, { x, y, z }) => newGrid.add(`${x},${y},${z}`), new Set(grid))
+export const plotDropletScan = (droplets, unplottedDropletScan) =>
+    droplets.reduce(
+        (plottedDropletScan, { x, y, z }) => plottedDropletScan.add(`${x},${y},${z}`),
+        new Set(unplottedDropletScan)
+    )
--- andrew/src/18/18.spec.js
+++ andrew/src/18/18.spec.js
@@ -1,10 +1,30 @@
-import { countUnconnectedSurfaces, parseDropletScan, plotDropletScan } from "@/18/18"
+import {
+    countExteriorSurfaces,
+    countUnconnectedSurfaces,
+    getSiblings,
+    getTrappedPockets,
+    isEnclosed,
+    parseDropletScan,
+    plotDropletScan,
+} from "@/18/18"
 import { readFileSync } from "fs"
 import { resolve } from "path"
 
 const EXAMPLE = String(readFileSync(resolve(__dirname, "18.example.txt")))
 
 const INPUT = String(readFileSync(resolve(__dirname, "18.input.txt")))
+
+describe("countExteriorSurfaces", () => {
+    it("should return the exterior surface area in a plotted droplet scan", () => {
+        expect(
+            countExteriorSurfaces(plotDropletScan(parseDropletScan(EXAMPLE), new Set()))
+        ).toEqual(58)
+
+        expect(countExteriorSurfaces(plotDropletScan(parseDropletScan(INPUT), new Set()))).toEqual(
+            2486
+        )
+    })
+})
 
 describe("countUnconnectedSurfaces", () => {
     it("should return the unconnected surface area in a plotted droplet scan", () => {
@@ -15,6 +35,162 @@ describe("countUnconnectedSurfaces", () => {
         expect(
             countUnconnectedSurfaces(plotDropletScan(parseDropletScan(INPUT), new Set()))
         ).toEqual(4418)
+
+        expect(
+            countUnconnectedSurfaces(
+                plotDropletScan(
+                    [
+                        { x: 1, y: 1, z: 1 },
+                        { x: 2, y: 1, z: 1 },
+                        { x: 2, y: 2, z: 1 },
+                        { x: 1, y: 1, z: 2 },
+                        { x: 1, y: 2, z: 2 },
+                        { x: 2, y: 2, z: 2 },
+                    ],
+                    new Set()
+                )
+            )
+        ).toEqual(24)
+    })
+})
+
+describe("getSiblings", () => {
+    it("should return the sibling coordinates", () => {
+        expect(getSiblings({ x: 1, y: 2, z: 3 })).toEqual([
+            { x: 1, y: 3, z: 3 },
+            { x: 1, y: 1, z: 3 },
+            { x: 2, y: 2, z: 3 },
+            { x: 0, y: 2, z: 3 },
+            { x: 1, y: 2, z: 4 },
+            { x: 1, y: 2, z: 2 },
+        ])
+    })
+})
+
+describe("getTrappedPockets", () => {
+    it("should return the trapped pockets in a plotted droplet scan", () => {
+        expect(getTrappedPockets(plotDropletScan(parseDropletScan(EXAMPLE), new Set()))).toEqual(
+            new Set(["2,2,5"])
+        )
+
+        const cube5 = Array.from(Array(5), (_, i) => i).flatMap((x) =>
+            Array.from(Array(5), (_, j) => j).flatMap((y) =>
+                Array.from(Array(5), (_, k) => k).map((z) => ({ x, y, z }))
+            )
+        )
+
+        expect(getTrappedPockets(plotDropletScan(cube5, new Set()))).toEqual(new Set())
+
+        expect(
+            getTrappedPockets(
+                plotDropletScan(
+                    cube5.filter(({ x, y, z }) => x !== 2 || y !== 2 || z !== 2),
+                    new Set()
+                )
+            )
+        ).toEqual(new Set(["2,2,2"]))
+
+        expect(
+            getTrappedPockets(
+                plotDropletScan(
+                    cube5.filter(
+                        ({ x, y, z }) =>
+                            x === 0 || y == 0 || z === 0 || x === 4 || y == 4 || z === 4
+                    ),
+                    new Set()
+                )
+            )
+        ).toEqual(
+            new Set([
+                "1,1,1",
+                "2,1,1",
+                "3,1,1",
+                "1,2,1",
+                "2,2,1",
+                "3,2,1",
+                "1,3,1",
+                "2,3,1",
+                "3,3,1",
+                "1,1,2",
+                "2,1,2",
+                "3,1,2",
+                "1,2,2",
+                "2,2,2",
+                "3,2,2",
+                "1,3,2",
+                "2,3,2",
+                "3,3,2",
+                "1,1,3",
+                "2,1,3",
+                "3,1,3",
+                "1,2,3",
+                "2,2,3",
+                "3,2,3",
+                "1,3,3",
+                "2,3,3",
+                "3,3,3",
+            ])
+        )
+
+        expect(
+            getTrappedPockets(
+                plotDropletScan(
+                    cube5.filter(
+                        ({ x, y, z }) =>
+                            ![
+                                "1,1,1",
+                                "2,1,1",
+                                "3,1,1",
+                                "3,2,1",
+                                "3,3,1",
+                                "2,3,1",
+                                "1,3,1",
+                            ].includes(`${x},${y},${z}`)
+                    ),
+                    new Set()
+                )
+            )
+        ).toEqual(new Set(["1,1,1", "2,1,1", "3,1,1", "3,2,1", "3,3,1", "2,3,1", "1,3,1"]))
+
+        expect(
+            getTrappedPockets(
+                plotDropletScan(
+                    cube5.filter(
+                        ({ x, y, z }) =>
+                            ![
+                                "1,1,1",
+                                "2,1,1",
+                                "3,1,1",
+                                "3,2,1",
+                                "3,3,1",
+                                "2,3,1",
+                                "1,3,1",
+                                "0,3,1",
+                            ].includes(`${x},${y},${z}`)
+                    ),
+                    new Set()
+                )
+            )
+        ).toEqual(new Set())
+    })
+})
+
+describe("isEnclosed", () => {
+    it("should return true for a coordinate that is enclosed", () => {
+        const cube5 = Array.from(Array(5), (_, i) => i).flatMap((x) =>
+            Array.from(Array(5), (_, j) => j).flatMap((y) =>
+                Array.from(Array(5), (_, k) => k).map((z) => ({ x, y, z }))
+            )
+        )
+
+        expect(
+            isEnclosed(
+                { x: 2, y: 2, z: 2 },
+                cube5.filter(
+                    ({ x, y, z }) => x === 0 || y == 0 || z === 0 || x === 4 || y == 4 || z === 4
+                )
+            )
+        ).toEqual(true)
     })
 })
 
