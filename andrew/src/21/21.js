8f22a5949d34e26c34cac85296b8217342f004c6
andrewbrennanfr <20241062+andrewbrennanfr@users.noreply.github.com>
2022-12-22 11:21:49 +0100

    Brute force part 2

--- andrew/src/21/21.js
+++ andrew/src/21/21.js
@@ -1,7 +1,9 @@
 const { readFileSync } = require("fs")
 const { resolve } = require("path")
 
-const getRootValue = (object) => {
+const getRootValue = (obj) => {
+    const object = { ...obj }
+
     while (Object.values(object).some((job) => typeof job === "string")) {
         Object.entries(object).forEach(([monkey, job]) => {
             const result = eval(job)
@@ -13,6 +15,48 @@ const getRootValue = (object) => {
     }
 
     return object.root
+}
+
+const getRootValue2 = (obj) => {
+    const initialObject = { ...obj }
+
+    Object.entries(initialObject).forEach(([monkey, job]) => {
+        if (monkey === "root") {
+            initialObject.root = job.replace(" + ", " === ")
+        }
+    })
+
+    let object = { ...initialObject }
+    let counter = 0
+
+    while (Object.values(object).some((job) => typeof job === "string") && object.root !== true) {
+        object.humn = counter
+
+        Object.entries(object).forEach(([monkey, job]) => {
+            const result = eval(job)
+
+            if (typeof result === "number" && !isNaN(result) && monkey !== "humn") {
+                object[monkey] = result
+            }
+
+            if (
+                monkey === "root" &&
+                typeof result === "boolean" &&
+                Object.entries(object)
+                    .filter(([monkey]) => monkey !== "root")
+                    .every(([, job]) => typeof job === "number")
+            ) {
+                object[monkey] = result
+            }
+        })
+
+        if (typeof object.root === "boolean" && object.root === false) {
+            counter = counter + 1
+            object = { ...initialObject }
+        }
+    }
+
+    return object.humn
 }
 
 const parseInput = (input) =>
@@ -73,3 +117,6 @@ const INPUT = parseInput(String(readFileSync(resolve(__dirname, "21.input.txt"))
 
 console.warn(getRootValue(EXAMPLE), getRootValue(EXAMPLE) === 152)
 console.warn(getRootValue(INPUT), getRootValue(INPUT) === 21120928600114)
+
+console.warn(getRootValue2(EXAMPLE), getRootValue2(EXAMPLE) === 301)
+// console.warn(getRootValue2(INPUT))
